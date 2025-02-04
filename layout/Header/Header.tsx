5ed05d16f37f0370bf0397c7b31ec43a48fb03ed
Bivash Kundu <bivash.kundu@webskitters.com>
2025-02-04 19:18:07 +0530

    contact section working

--- layout/Header/Header.tsx
+++ layout/Header/Header.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable react/no-unescaped-entities */
+import assest from "@/json/assest";
 import { sociallinks } from "@/json/mock/common.mock";
 import {
   HeaderDrawer,
@@ -18,6 +19,7 @@ import {
   Stack,
   Typography
 } from "@mui/material";
+import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
@@ -103,12 +105,12 @@ const Header = () => {
           <Grid container alignItems="center">
             <Grid item xs={6} lg={2}>
               <Link rel="noreferrer" aria-label="testlabel" href="/">
-                {/* <Image
+                <Image
                   src={assest.logo_img}
                   width={200}
                   height={76}
                   alt="logo"
-                /> */}
+                />
               </Link>
             </Grid>
             <Grid item xs={6} lg={7.5}>
